0d141c60a5448d3446af6a6c16cb9eb9630478b9
DevMilim <devmilim@gmail.com>
2025-08-21 21:40:12 -0300

    change server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,18 +1,14 @@
-use std::{fs, path::PathBuf, sync::RwLock};
+use std::{fs, path::PathBuf};
 
 use app::{ServerApp, ServerAppProps};
 use rocket::{
-    fairing::{Fairing, Info, Kind},
     fs::FileServer,
     get,
     response::content::RawHtml,
-    routes, Data, Request,
+    routes,
 };
 use yew::ServerRenderer;
 
-struct AppState {
-    counter: RwLock<i32>,
-}
 
 #[get("/<url..>", rank = 20)]
 async fn yew_routes(url: PathBuf) -> Option<RawHtml<String>> {
@@ -34,34 +30,8 @@ async fn yew_routes(url: PathBuf) -> Option<RawHtml<String>> {
     Some(RawHtml(a))
 }
 
-struct Counter {
-    get: RwLock<i32>,
-}
-
-#[rocket::async_trait]
-impl Fairing for Counter {
-    fn info(&self) -> Info {
-        Info {
-            name: "GET Counter",
-            kind: Kind::Request | Kind::Response,
-        }
-    }
-    async fn on_request(&self, request: &mut Request<'_>, _: &mut Data<'_>) {
-        println!("Request: {:?}", self.get.read());
-        let mut value = self.get.write().unwrap();
-        *value += 1;
-    }
-}
-
 pub async fn run_server() -> Result<(), rocket::Error> {
-    let a = AppState {
-        counter: RwLock::new(0),
-    };
     let _ = rocket::build()
-        .manage(a)
-        .attach(Counter {
-            get: RwLock::new(0),
-        })
         .mount("/", FileServer::from("static"))
         .mount("/", routes![yew_routes])
         .launch()
